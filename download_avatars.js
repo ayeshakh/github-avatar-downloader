ab2c559e2b0fdaa46d32164441dd0766cdcda977
Ayesha <ayesha_04@yahoo.com>
2017-07-03 19:51:34 +0000

    My third commit for the project

--- download_avatars.js
+++ download_avatars.js
@@ -2,11 +2,16 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = "YOUR USERNAME HERE";
+var GITHUB_TOKEN = "YOUR ACCESSTOKEN HERE";
 function getRepoContributors(repoOwner, repoName, cb) {
 
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
