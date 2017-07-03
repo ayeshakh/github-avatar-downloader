77478976ffd2d9ae78f2a5d8ba19e4c943620774
Ayesha <ayesha_04@yahoo.com>
2017-07-03 23:29:28 +0000

    Step 8 avatar pictures

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 
   console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -29,12 +30,27 @@ var request = require('request');
 
     var contributors = JSON.parse(body);
     contributors.forEach(function(contributor){
-    console.log('Avatar URL:', contributor.avatar_url);
+      console.log('Avatar URL:', contributor.avatar_url);
+      downloadImageByURL(contributor.avatar_url, './avatars/' + contributor.login + ".jpg");
     });
 
-  });
+   });
 
 };
+
+  function downloadImageByURL(url, filepath) {
+    request.get(url)               // Note 1
+    .on('error', function (err) {                                   // Note 2
+     throw err;
+    })
+    .on('response', function (response) {                           // Note 3
+     console.log('Response Status Code: ', response.statusCode);
+    })
+    .pipe(fs.createWriteStream(filepath));
+  }
+
+
+
 
   getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("Errors:", err);
